ba1bd0d720c6c9497306ce77093c33e328742a22
Kaushik Mahadevan <kaushik.mahadevan@gmail.com>
2021-05-27 02:41:07 -0400

    part 1

--- App.tsx
+++ App.tsx
@@ -3,6 +3,9 @@ import {Image, StyleSheet, Text, View} from 'react-native';
 // App.js
 import React, {Component} from 'react';
 
+import {Dialogflow_V2} from 'react-native-dialogflow';
+import {dialogflowConfig} from './src/env';
+
 const styles = StyleSheet.create({
   bubble: {
     flex: 1,
@@ -24,7 +27,11 @@ interface Message {
 interface State {
   messages: Array<Message>;
 }
-
+const BOT = {
+  _id: 2,
+  name: 'FAQ Bot',
+  avatar: 'https://i.imgur.com/7k12EPD.png',
+};
 class App extends Component {
   state: State = {
     messages: [
@@ -32,18 +39,49 @@ class App extends Component {
         _id: 1,
         text: `Hi! I am the FAQ bot 🤖 from Jscrambler.\n\nHow may I help you with today?`,
         createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'FAQ Bot',
-          avatar: 'https://i.imgur.com/7k12EPD.png',
-        },
+        user: BOT,
       },
     ],
   };
 
-  onSend(messages = []) {
+  componentDidMount() {
+    Dialogflow_V2.setConfiguration(
+      dialogflowConfig.client_email,
+      dialogflowConfig.private_key,
+      Dialogflow_V2.LANG_ENGLISH_US,
+      dialogflowConfig.project_id,
+    );
+  }
+
+  onSend(messages: Array<Message> = []) {
     this.setState((previousState: State) => ({
       messages: GiftedChat.append(previousState.messages, messages),
+    }));
+
+    let message = messages[0].text;
+    Dialogflow_V2.requestQuery(
+      message,
+      result => this.handleGoogleResponse(result),
+      error => console.log(error),
+    );
+  }
+
+  handleGoogleResponse(result: any) {
+    const text = result.queryResult.fulfillmentMessages[0].text.text[0];
+    console.log(text);
+    this.sendBotResponse(text.length > 0 ? text : 'hmm not sure');
+  }
+
+  sendBotResponse(text: string) {
+    let msg = {
+      _id: this.state.messages.length + 1,
+      text,
+      createdAt: new Date(),
+      user: BOT,
+    };
+
+    this.setState((previousState: State) => ({
+      messages: GiftedChat.append(previousState.messages, [msg]),
     }));
   }
 
---
+++ src/env.ts
@@ -0,0 +1,14 @@
+export const dialogflowConfig = {
+  type: 'service_account',
+  project_id: 'faq-bot-waug',
+  private_key_id: '1ad128d29112b76000323bf38932ea25968a6c14',
+  private_key:
+    '-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDNibJA2z4lywBD\n3dpDbl8BlxF4co9zbcCBTIhNNQRbwM/CWmQqJAyBelh5x7zarpMvzJr41DJf8J38\ni0FgfOQH0wEHabkugGlT7MVM7bQ/i6aO8vrRN1IMriZVu1YkpD1xlnWox3FIQWlN\nerAd7BCkKkTyI8yQRHtGIC0BXNi1g+Z+xU1nf5oOere9UVQqH6NP9k1y/1AZskE8\n+yDsW0E6xGwbokN9teSNva7U/hqVD7XAm5PUEprbNpjDQEDGmmyejENbKVsWBBbj\n9DstiM8Phx2Mc5HMdPFCRS2wAC/dNSxvE3FP9o/jx7k+fLe5KCUL8izyfojXFcQ8\nIhXRaivDAgMBAAECggEAZOa9MGOxHcKLjcWiucm8MD6mX/f1flgZ0h/tJeHqEtUT\n6TiQxbNOxF14g8hPaRecwDF0vsUFOd4Y4PrIDIMjOsgWUDpRo4d8raulrD9hZnS7\nPqAAC6dZy0nPHfVKs9BBLbsMZeALN8qM1HFvdjEGdmEd2bVDXmuQDPwMvAeFh42o\nJTa2sB65xe/9SjBwH1+wV0si6JW5fgrGfQKWB6zuri+pvhd5Eg958HMZs0mCXkUe\nsXU6eBy5PgPvCU6Qwu2y4DC/aqMUA2+vBGX3yOgKZ7nrMUTlUTvMbXdupXw+8QGy\nvAUABDlKFrttQYPY6hI4ztgs7tFyRhvzQ/umRkUD+QKBgQD5EFJ49aNHgplxkGuJ\nNwo86j1Z7YFYksjoDk7l97LTZ3dss9qb8GbuiCh20fdrK7THe7KIzs6yP1qu1RkY\nwKyCDmSYdyEmLss/2FxXhMh0jKfJPeHsa/FDKWj31Jl37Awp+Rp2uauKQnyGBdS+\n1JAKJoRTFOxpz5sQCxAv8jZ7pQKBgQDTQw9sj0pL9/o7vgsJdAEOZraaMtZ82yZj\n3si5U4+n9UpPDHzbieYEMZvKQhFPfeuMl7YYVn0VreNee3TKxXdXxgjgjLE5RHrz\nc3jKvSmVCWP7F3tmzWGB7QCe4cSHJ+AR7GoUQSirUXD63Mq9p2NRxFwqGIdtIWrT\no2FvEySNRwKBgQC0y1FitsSowzfJhRFakJag81RHtC1hc2Pdd410R+MKrJ+3S7NT\n4SWG31WoMvBMXnSjHbrSrnTCVVTpdoz6N9rBqdsCBfANPIiTtuYJLb0pRXUCkyYt\nrYl7vFKTHEOvFq0Zxtab+ll4Sp0D2LW/NKO+iokhmh2nDWm9Mpq0gzljdQKBgFUv\ngzapCLJdSX9xgA3NwudFr3e57BboXzCCErn34cM9+zqotGfp0N/8CO+jeEtvYN2O\nBxUFOubU+QuOCGgC+75e0hGJaaTH4jPsadaGNBAueywyKcYoJps3L7rhGlfNUmKn\ny5ZjN7zs1/uvr+iNaFT+MpOe1GzCvsqb5JvfQivdAoGBAIwqv6hCxG/5D2tGX+r6\nYzebIhItDB8tAEfaX0Z1Q4Nrdhu0hfAQDr0jnvz2BnNrCyXicnOAui4KnEtS90nV\nZY6nkvpJrXbVa8pU4zcsM1j5gWDuCx4jFMvXKU/CzJpkUA3ovnGKzCSlnvnKwWAJ\nnr/qBq8Nr8TNIQtFbw6tSOHt\n-----END PRIVATE KEY-----\n',
+  client_email: 'fas-bot-acc@faq-bot-waug.iam.gserviceaccount.com',
+  client_id: '114591147385400275805',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/fas-bot-acc%40faq-bot-waug.iam.gserviceaccount.com',
+};
